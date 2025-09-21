e64f09953442e9c2221647b6ea28097998a2502d
Georgiy Mikhailenko <kislball@yahoo.com>
2025-09-21 12:02:52 +0300

    Implement sorting

---
+++ src/sorting/sorting.py
@@ -0,0 +1,24 @@
+# Функция сортирует список in-place
+def sort(ls):
+    for i in range(len(ls)):
+        swap = False
+
+        for j in range(0, len(ls) - i - 1):
+            if ls[j] > ls[j + 1]:
+                ls[j], ls[j + 1] = ls[j + 1], ls[j]
+                swap = True
+
+        if not swap:
+            break
+
+if __name__ == "__main__":
+    l = int(input("Введите длину списка: "))
+    ls = []
+
+    for i in range(l):
+        ls.append(int(input(f"Введите элемент номер {i + 1}: ")))
+
+    sort(ls)
+
+    print(' '.join(str(x) for x in ls))
+
