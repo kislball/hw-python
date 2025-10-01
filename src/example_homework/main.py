daed80bc1fcfe27f40c15f4bf991bd014762147b
Georgiy Mikhailenko <kislball@yahoo.com>
2025-10-01 20:01:34 +0300

    Add documentation for example_homework

--- src/example_homework/main.py
+++ src/example_homework/main.py
@@ -1,24 +1,32 @@
 from abc import ABC, abstractmethod
 
-
+# Класс GreetingProvider является интерфейсом для различных провайдеров строк приветствия.
+# Чаще всего используется для поддержки мультиязычности, различных уровней вежиловсти и т.д.
 class GreetingProvider(ABC):
     @abstractmethod
+    # get_greeting возвращает приветствие для имени name.
     def get_greeting(self, name):
         pass
 
+# Реализация GreetingProvider для русского языка
 class RussianGreetingProvider(GreetingProvider):
     def get_greeting(self, name):
         return f"Привет, {name}!"
 
+# Реализация GreetingProvider для английского языка
 class EnglishGreetingProvider(GreetingProvider):
     def get_greeting(self, name):
         return f"Hello, {name}!"
 
+# Интерфейс для различных исполнителей приветствий. Например ConsoleGreetingExecutor.
+# Также возможна реализация иных каналов связи, например голосовой(условный VoiceGreetingExecutor) и пр.
 class GreetingExecutor(ABC):
     @abstractmethod
-    def greet_using_provider(self, provider):
+    # Исполняет приветствия, предоставляемое GreetingProvider.
+    def greet_using_provider(self, provider, name):
         pass
 
+# Реализация GreetingExecutor для stdio.
 class ConsoleGreetingExecutor(GreetingExecutor):
     def greet_using_provider(self, provider, name):
         print(provider.get_greeting(name))
