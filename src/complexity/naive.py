3b6904d3522894009c14648811cad602d60061f2
Егор <kislball@yahoo.com>
2025-10-05 20:12:55 +0300

    Оформление задачи (#4)
    * Added pyproject.toml with basic linting configuration
    
    * Format existing homework
    
    * Rename functions

--- src/complexity/naive.py
+++ src/complexity/naive.py
@@ -1,7 +1,16 @@
+"""Переборное решение задачи с ферзями.
+
+Перебирвет все перестановки ферзей по колонкам, проверяет каждую на наличие конфликтов.
+При запуске напрямую выводит решение для числа, заданного через stdin.
+
+Более подробное описание сложности содержится в файле complexity.md.
+"""
+
 import itertools
 
 
 def check_field(queens: list[int]):
+    """Проверяет наличие ферзей, атакуемых другими ферзями, на данном поле."""
     n = len(queens)
     for i in range(n):
         for j in range(i + 1, n):
@@ -10,7 +19,8 @@ def check_field(queens: list[int]):
     return True
 
 
-def solve(n):
+def solve_naive(n):
+    """Решает задачу о расположении n ферзей на шахматной доске размера n*n."""
     count = 0
     for perm in itertools.permutations(range(n)):
         if check_field(perm):
@@ -20,4 +30,4 @@ def solve(n):
 
 if __name__ == "__main__":
     n = int(input())
-    print(solve(n))
+    print(solve_naive(n))
